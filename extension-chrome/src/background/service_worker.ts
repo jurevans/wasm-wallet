ca16b402ee36d986e32349bf1b36cd5ae60b807c
Justin Evans <jurevans@gmail.com>
2021-12-30 19:21:59 -0500

    Adding basic struct, impl

--- extension-chrome/src/background/service_worker.ts
+++ extension-chrome/src/background/service_worker.ts
@@ -1,17 +1,22 @@
 import wasm from '../../pkg';
 
 chrome.runtime.onInstalled.addListener(async () => {
+  const { loaded_message } = await wasm;
+  loaded_message();
   console.log('background listener: Extension installed successfully!');
 });
 
 chrome.runtime.onConnect.addListener(async (port) => {
+  const { Wallet } = await wasm;
+  const wallet = Wallet.new();
+
   if (port.name === 'walkietalkie') {
-    const { Wallet } = await wasm;
-    Wallet.loaded_message();
     port.onMessage.addListener((msg) => {
       const { type, data } = msg;
       switch (type) {
         case 'check':
+          wallet.set_address('TEST_ADDRESS');
+          console.log({ address: wallet.get_address() });
           console.info({ message: data.check });
           port.postMessage({
             type: 'response',
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -13,13 +13,53 @@ macro_rules! console_log {
 }
 
 #[wasm_bindgen]
-struct Wallet {
-  passphrase: String,
+pub struct Wallet {
+    address: String,
+}
+
+#[wasm_bindgen]
+pub struct Account {
+    key: String,
+    accounts: Vec<Wallet>,
 }
 
 #[wasm_bindgen]
 impl Wallet {
-    pub fn loaded_message() {
-        console_log!("WASM has been successfully loaded!")
+    pub fn new() -> Self {
+        utils::set_panic_hook();
+        Self {
+            address: "".to_string()
+        }
     }
+
+    pub fn set_address(&mut self, address: &str) {
+        self.address = address.to_string()
+    }
+
+    pub fn get_address(&self) -> String {
+        self.address.to_string()
+    }
+}
+
+#[wasm_bindgen]
+impl Account {
+    pub fn new(key: &str) -> Self {
+        Self {
+            key: key.to_string(),
+            accounts: Vec::new(),
+        }
+    }
+
+    pub fn get_key(&self) -> String {
+        self.key.to_string()
+    }
+
+    pub fn add_account(&mut self, wallet: Wallet) {
+        self.accounts.push(wallet)
+    }
+}
+
+#[wasm_bindgen]
+pub fn loaded_message() {
+    console_log!("WASM has been successfully loaded!")
 }
